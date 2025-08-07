972b13fe718f7fccaea403c1770dd105a257a96e
Wusunanwa <kennedywusunanwar@gmail.com>
2025-08-07 18:25:30 +0000

    fix: update supabase imports after moving to src/

--- src/pages/ListingDetail.jsx
+++ src/pages/ListingDetail.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams, useNavigate } from "react-router-dom";
-import { supabase } from "../../supabaseClient";
+import { supabase } from "../supabaseClient";
 import Logo from "../assets/logo.png";
 
 
